0f88a6beb23f3909adb86f3db71c4bb6fee65a10
gaoxing12 <1570275769@qq.com>
2019-04-30 13:58:54 +0800

    首页已完成（加入弹性盒子，头部的效果消失）

--- js/main.js
+++ js/main.js
@@ -16,5 +16,41 @@ $(function(){
 			$(".saoyisao").children("span").css({"color":"#fff"});
 		}
 //		console.log(gHidtop);
-	})
+	});
+	
+	
+//	二维码扫一扫
+	$(".scan").bind("tap",function(e){
+		e.preventDefault();
+		mui.openWindow({
+			url:"erweima.html",
+			id:"scan",
+			styles:{
+				duration:300
+			}
+		});
+	});
+	
+//	点击头部的会员码,出现登录页面
+	$(".vip").bind("tap",function(e){
+		e.preventDefault();
+		mui.openWindow({
+			url:"html/denglu.html",
+			id:"vip",
+			styles:{
+				duration:1000
+			},
+			show:{
+				aniShow:'slide-in-bottom'
+			}
+		});
+	});
+//	弹性窗口
+(function($){
+        $(".mui-scroll-wrapper").scroll({
+              bounce: true,//滚动条是否有弹力默认是true
+              indicators: false, //是否显示滚动条,默认是true
+//      	$('.mui-bar-transparent').transparent();
+        });
+})(mui);
 });
